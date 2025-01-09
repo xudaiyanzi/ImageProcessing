1b7098461237260aae6ff98b324baac4256d3dc2
xudaiyanzi <xudaiyanzi@gmail.com>
2025-01-08 23:51:37 -0600

    revise the video script

--- step4_readVideo.py
+++ step4_readVideo.py
@@ -7,12 +7,12 @@ capture = cv2.VideoCapture(video_path)
 ret = True
 while ret:
     ret, frame = capture.read() # Read the video frame by frame
-    cv2.imshow('Video', frame)
-
+    
     # wait time is calculated based on the frames per second of the video
     # if an video is 50fps, then 1000ms/50fps = 20ms, so we need to wait 20ms to get the next frame
-    if cv2.waitKey(20) & 0xFF == ord('d'): # Press 'd' to break the loop.
-        break
+    if ret:
+        cv2.imshow('Video', frame)
+        cv2.waitKey(20) # wait for 20ms before the next frame is displayed
 
 capture.release() # Release the video capture object from the memory
 cv2.destroyAllWindows()
