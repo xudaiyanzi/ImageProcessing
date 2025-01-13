d85dcf4d46a2866f5b3c66be0b271d01b83eb59f
xudaiyanzi <xudaiyanzi@gmail.com>
2025-01-13 13:42:57 -0600

    blur practice

--- step3_sharpAndBlur/step3_blur_basic.py
+++ step3_sharpAndBlur/step3_blur_basic.py
@@ -12,9 +12,17 @@ img_g_blur = cv2.GaussianBlur(img_original, (k_size, k_size), 0)
 ## just blur the image, not guassian blur
 img_blur = cv2.blur(img_original, (k_size, k_size))
 
+## blur the image with median filter
+img_median = cv2.medianBlur(img_original, k_size)
+
+## blur the image with bilateral filter, it can keep the edge of the image
+img_bilateral = cv2.bilateralFilter(img_original, 9, 75, 75)
+
 cv2.imshow('Original', img_original)
 cv2.imshow('Gaussian Blur', img_g_blur)
 cv2.imshow('Blur', img_blur)
+cv2.imshow('Median', img_median)
+cv2.imshow('Bilateral', img_bilateral)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
