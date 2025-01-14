2f392c08c27648f9c2fdaa95ef890989c8196855
xudaiyanzi <xudaiyanzi@gmail.com>
2025-01-13 23:41:45 -0600

    add the box

--- step7_contour/step7_contour.py
+++ step7_contour/step7_contour.py
@@ -22,14 +22,20 @@ ret, binary_inv = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
     ### cv2.CHAIN_APPROX_NONE: stores absolutely all the contour points. That is, any 2 subsequent points (x1, y1) and (x2, y2) of the contour will be either horizontal, vertical or diagonal neighbors, that is, max(abs(x1-x2), abs(y2-y1))==1
     ### cv2.CHAIN_APPROX_SIMPLE: compresses horizontal, vertical, and diagonal segments and leaves only their end points. For example, an up-right rectangular contour is encoded with 4 points
     ### cv2.CHAIN_APPROX_TC89_L1: applies one of the flavors of the Teh-Chin chain approximation algorithm, which merges similar regions and reduces the number of points in a curve
+    
+## cv2.findContours() returns 2 values: contours, hierarchy
+## contours: a list of contours
+## hierarchy: optional output vector containing information about the image topology. It has as many elements as the number of contours. 
+    # For each i-th contour contours[i], the elements hierarchy[i][0], hierarchy[i][1], hierarchy[i][2], 
+    # and hierarchy[i][3] are set to 1, 2, 3, and 4, respectively.
 contours, hierarchy = cv2.findContours(binary_inv, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 ## print contours which is larger than 1000
 for contour in contours:
     if cv2.contourArea(contour) > 1000:
         # print('contour area:', cv2.contourArea(contour))
-        ## draw the contour
-        ## cv2.drawContours(image, contours, contourIdx, color, thickness)
+        # # draw the contour
+        # # cv2.drawContours(image, contours, contourIdx, color, thickness)
         # cv2.drawContours(img, [contour], -1, (0, 255, 0), 3)
         
         ## find bounding box of contour
